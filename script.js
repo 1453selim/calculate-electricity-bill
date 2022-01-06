8b814006a93e88800d4cdc4ba2aa1e95a2a34c49
selim isabetli <81588521+1453selim@users.noreply.github.com>
2022-01-06 14:10:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//2022 Elektirk Faturası Hesaplama
+
+let kwh = prompt('Tükettiğiniz "KWH" miktarını giriniz');
+
+
+
+
+function fatura(val) {
+
+  if (val > 150) {
+
+    aktif = (150 * 0.7946) + ((val - 150) * 1.3488)
+
+  } else if (val > 0 && val <= 150) {
+
+    aktif = (val * 0.7946)
+
+  } else {
+
+    return console.log(`Hatalı Giriş Yaptınız`)
+  }
+
+  dagitim = (val * 0.3295)
+  tuketim = aktif + dagitim
+
+  enerji = aktif * 0.007
+  btv = aktif * 0.04
+  kdv = (tuketim + enerji + btv) * 0.18
+
+  total = tuketim + btv + kdv + enerji
+
+  console.warn(`${kwh} kwh Tüketim İçin Fatura Ayrıntıları`)
+
+  console.log(`Aktif Enerji Bedeli      ${(aktif).toFixed(2)} TL`)
+  console.log(`Dağıtım Bedeli            ${(dagitim).toFixed(2)} TL`)
+  console.warn(`Toplam Tüketim Bedeli   ${(tuketim).toFixed(2)} TL`)
+
+  console.log(`Enerji Fonu                ${(enerji).toFixed(2)} TL`)
+  console.log(`BTV                        ${(btv).toFixed(2)} TL`)
+  console.log(`KDV                       ${(kdv).toFixed(2)} TL`)
+  console.warn(`Vergiler Toplamı         ${(enerji + btv + kdv).toFixed(2)} TL`)
+
+  console.warn(`Toplam Fatura           ${(total).toFixed(2)} TL`)
+}
+
+
+fatura(kwh);
